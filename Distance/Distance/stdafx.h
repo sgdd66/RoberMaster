5563f1976b68cd36b2edf5bfdf7c7f77224cdf16
上官栋 <1770480953@qq.com>
2018-03-10 18:00:59 +0800

    3/10

---
+++ Distance/Distance/stdafx.h
@@ -0,0 +1,10 @@
+#include<opencv2/opencv.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+#include<fstream>
+using namespace std;
